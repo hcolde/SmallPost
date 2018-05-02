ba31d4855a60372c431e0a8ee3095acc8d6aa4c3
hhy <532361351@qq.com>
2018-05-02 15:42:42 +0800

    完成查看自己发表的评论功能，可进行左右翻页

--- mysite/posts/static/posts/js/mine.js
+++ mysite/posts/static/posts/js/mine.js
@@ -1,15 +1,18 @@
-$("a[href='#Post']").bind("click", function() {
+/*$("a[href='#Post']").bind("click", function() {
 	$(this).unbind('click');
-});
+});*/
 
-function FillIn(data){
+function FillIn(data, btn){
 	$("div[class^='panel']").remove();
 	for(var i=0; i<data.title.length; i++){
 		var title = data.title[i];
 		var id = data.id[i];
 		var date = data.date[i];
 		var html = '<div class="panel panel-default"><div class="panel-heading" align="left">'+ date +'</div><div class="panel-body">'+ title +'</div></div>';
-		$("#Post").append(html);
+		if(btn=="POST")
+			$("#Post").append(html);
+		else
+			$("#Comment").append(html);
 	}
 }
 
